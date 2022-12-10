9d2e7c8de5ce33555b3694caca61e18667ac9167
Loan Nguyen <muatrenphobayxa115@gmail.com>
2022-12-10 17:51:35 +0700

    lesson 27, Checkout Page - step 1, 2, 3

---
+++ src/test/java/models/components/checkout/PaymentInformationComponent.java
@@ -0,0 +1,11 @@
+package models.components.checkout;
+
+import models.components.Component;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class PaymentInformationComponent extends Component {
+    public PaymentInformationComponent(WebDriver driver, WebElement component) {
+        super(driver, component);
+    }
+}
---
+++ src/test/java/models/components/checkout/PaymentMethodComponent.java
@@ -0,0 +1,11 @@
+package models.components.checkout;
+
+import models.components.Component;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class PaymentMethodComponent extends Component {
+    public PaymentMethodComponent(WebDriver driver, WebElement component) {
+        super(driver, component);
+    }
+}
